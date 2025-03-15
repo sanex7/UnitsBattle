9f4bf3b49ed970301833268685c17fc120855def
sanex7 <159659818+sanex7@users.noreply.github.com>
2025-03-15 11:38:47 +0200

    Update Archer.cpp(ISerializable)

--- Archer.cpp
+++ Archer.cpp
@@ -1,22 +1,43 @@
 #include "Archer.h"
+#include "ISerializable.h"
+#include <fstream>
 
 namespace SpecializedUnits {
 
-    Archer::Archer(const String<>& name, int HP, unsigned int arrows)
-        : Unit(name, HP), arrows(arrows) {}
+    std::ostream& Archer::Serialize(std::ostream& output) {
+        Unit::Serialize(output);
+        output << arrows << "\n";
+        return output;
+    }
 
-    void Archer::Attack(Unit& target) {
-        if (arrows > 0) {
-            std::cout << name << " shoots an arrow. Arrows left: " << --arrows << "\n";
-        }
-        else {
-            std::cout << name << " is out of arrows!\n";
+    std::istream& Archer::Deserialize(std::istream& input) {
+        Unit::Deserialize(input);
+        input >> arrows;
+        input.ignore();
+        return input;
+    }
+
+    void Archer::Serialize(const std::string& path) {
+        std::ofstream file(path);
+        if (file.is_open()) {
+            Serialize(file);
+            file.close();
         }
     }
 
-    void Archer::Defense() {
-        IsDefense = true;
-        std::cout << name << " dodges the attack.\n";
+    void Archer::Deserialize(const std::string& path) {
+        std::ifstream file(path);
+        if (file.is_open()) {
+            Deserialize(file);
+            file.close();
+        }
     }
 
+    void Archer::Serialize() {
+        std::cout << "Serializing Archer: " << name << ", HP: " << HP << ", Arrows: " << arrows << std::endl;
+    }
+
+    void Archer::Deserialize() {
+        std::cout << "Deserializing Archer: " << name << ", HP: " << HP << ", Arrows: " << arrows << std::endl;
+    }
 }
