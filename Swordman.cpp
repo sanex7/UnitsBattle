8464d226b731f4a5517e8627bbc69b4974fb4fbb
sanex7 <159659818+sanex7@users.noreply.github.com>
2025-03-15 11:39:30 +0200

    Update Swordman.cpp(ISerializable)

--- Swordman.cpp
+++ Swordman.cpp
@@ -1,18 +1,43 @@
 #include "Swordman.h"
+#include "ISerializable.h"
+#include <fstream>
 
 namespace SpecializedUnits {
 
-    Swordman::Swordman(const String<>& name, int HP, unsigned int power)
-        : Unit(name, HP), power(power) {}
-
-    void Swordman::Attack(Unit& target) {
-        std::cout << name << " attacks with sword power " << power << "!\n";
-        target.TakeDamage(power);
+    std::ostream& Swordman::Serialize(std::ostream& output) {
+        Unit::Serialize(output);
+        output << power << "\n";
+        return output;
     }
 
-    void Swordman::Defense() {
-        IsDefense = true;
-        std::cout << name << " defends with a shield.\n";
+    std::istream& Swordman::Deserialize(std::istream& input) {
+        Unit::Deserialize(input);
+        input >> power;
+        input.ignore();
+        return input;
     }
 
+    void Swordman::Serialize(const std::string& path) {
+        std::ofstream file(path);
+        if (file.is_open()) {
+            Serialize(file);
+            file.close();
+        }
+    }
+
+    void Swordman::Deserialize(const std::string& path) {
+        std::ifstream file(path);
+        if (file.is_open()) {
+            Deserialize(file);
+            file.close();
+        }
+    }
+
+    void Swordman::Serialize() {
+        std::cout << "Serializing Swordman: " << name << ", HP: " << HP << ", Power: " << power << std::endl;
+    }
+
+    void Swordman::Deserialize() {
+        std::cout << "Deserializing Swordman: " << name << ", HP: " << HP << ", Power: " << power << std::endl;
+    }
 }
